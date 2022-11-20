321274bedba09563614c4962767380eb4e162fa6
Yusuf Doria <yusufdoria123@gmail.com>
2022-11-20 00:06:18 -0500

    remodel

--- script.js
+++ script.js
@@ -1,44 +1,34 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-            scrollTop: $(hash).offset().top
-            }, 800, function(){
-            window.location.hash = hash;
-            });
-        } 
-    });
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
 
-    const sections = document.querySelectorAll("section[id]");
+window.addEventListener('DOMContentLoaded', event => {
 
-    function navHighlighter() {
-  
-        // Get current scroll position
-        let scrollY = window.pageYOffset;
-        
-        // Now we loop through sections to get height, top and ID values for each
-        sections.forEach(current => {
-          const sectionHeight = current.offsetHeight;
-          const sectionTop = current.offsetTop - 50;
-          sectionId = current.getAttribute("id");
-          
-          /*
-          - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-          - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-          */
-          if (
-            scrollY > sectionTop &&
-            scrollY <= sectionTop + sectionHeight
-          ){
-            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
-          } else {
-            document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector('#sideNav');
+  if (sideNav) {
+      new bootstrap.ScrollSpy(document.body, {
+          target: '#sideNav',
+          offset: 74,
+      });
+  };
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+      document.querySelectorAll('#navbarResponsive .nav-link')
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+      responsiveNavItem.addEventListener('click', () => {
+          if (window.getComputedStyle(navbarToggler).display !== 'none') {
+              navbarToggler.click();
           }
-        });
-      }
-      
+      });
+  });
 
-    window.addEventListener("scroll", navHighlighter);
 });
